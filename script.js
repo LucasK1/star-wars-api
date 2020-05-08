c729cee73a522e508f76253b76cf74d674869624
Łukasz Kozłowski <ryuke12@gmail.com>
2020-05-08 22:45:48 +0200

    Update script.js

--- script.js
+++ script.js
@@ -4,18 +4,7 @@ let requestedItems = [];
 
 document.getElementById('get-info').addEventListener('click', loadRequest);
 
-// Gets value of the active radio button
 
-const getRadioValue = () => {
-  let radios = document.getElementsByName('type-of-request');
-
-  for (i= 0; i < radios.length; i++) {
-    if (radios[i].checked) {
-      console.log(radios[i]);
-      return radios[i].value;
-    }
-  }
-}
 
 // Passes to the request the number and type of requested items
 
@@ -25,15 +14,17 @@ function loadRequest(e) {
   let numberRequested = document.getElementById('number').value;
   let typeRequested = getRadioValue();
   let i;
+
   for (i = 1; i <= numberRequested; i++) {
-    let randomNumber = Math.floor(Math.random() * 20);
+    let randomNumber = Math.ceil(Math.random() * 10);
+    console.log(randomNumber);
     getInformation(randomNumber, typeRequested);
   }
 }
 
 // Gets specified number and type of items from the API
 
-const getInformation = (number, type) => {
+function getInformation (number, type) {
   fetch(`https://swapi.dev/api/${type}/${number}`)
   .then(res => res.text())
   .then(data => JSON.parse(data))
@@ -41,18 +32,30 @@ const getInformation = (number, type) => {
   .catch(err => console.log(err));
 }
 
+// Gets value of the active radio button
 
-function displayRequestedPerson(person) {
-  let name = document.createTextNode(person.name);
-  let birthYear = document.createTextNode(person.birth_year);
-  document.querySelector('.container').appendChild(ul).appendChild(li).appendChild(name);
-}
+function getRadioValue() {
+  let radios = document.getElementsByName('type-of-request');
 
-
-
-function handleErrors(response) {
-  if (!response.ok) {
-      throw Error(response.statusText);
+  for (i= 0; i < radios.length; i++) {
+    if (radios[i].checked) {
+      return radios[i].value;
+    }
   }
-  return response;
 }
+
+function testDisplay(arr) {
+  let container = document.getElementById('display');
+  container.innerText = arr;
+}
+
+function displayArray() {
+  let a = setInterval(() => {
+    if (requestedItems.length > 0) {
+      testDisplay(requestedItems[1].name);
+      clearInterval(a);
+    }
+}, 1000);
+}
+
+displayArray();
